620f5f6b13319964890e407e1fa5a28d8152e7b1
David Ramirez <davidraviles@gmail.com>
2019-06-17 00:33:20 -0700

    switched html files to php

---
+++ illustration/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="description" content="The work of David Ramirez, designer & developer, for web, photography, illustrations, and more.">
+        <?php include ($_SERVER['DOCUMENT_ROOT'].'/head.php');?>
+    </head>
+    <body>
+        <?php include ($_SERVER['DOCUMENT_ROOT'].'/titlebar.php');?>
+        <?php include ($_SERVER['DOCUMENT_ROOT'].'/nav.php');?>
+        <div id="container">
+        </div>
+        <footer>&copy; Copyright 2019. All Rights Reserved.</footer>
+        <?php include ($_SERVER['DOCUMENT_ROOT'].'/scripts.php');?>
+    </body>
+</html>
