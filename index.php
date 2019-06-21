a80e603251761fac99b2567de63788880fd633aa
David Ramirez <davidraviles@gmail.com>
2019-06-21 00:57:07 -0700

    Added language and loading spinner.

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
     <head>
         <meta name="description" content="The work of David Ramirez, designer & developer, for web, photography, illustrations, and more.">
         <?php include ($_SERVER['DOCUMENT_ROOT'].'/head.php');?>
@@ -8,6 +8,7 @@
         <?php include ($_SERVER['DOCUMENT_ROOT'].'/titlebar.php');?>
         <?php include ($_SERVER['DOCUMENT_ROOT'].'/nav.php');?>
         <div id="container">
+        <i class="m-spinner--signal"></i>
         </div>
         <footer>&copy; Copyright 2019. All Rights Reserved.</footer>
         <?php include ($_SERVER['DOCUMENT_ROOT'].'/scripts.php');?>
