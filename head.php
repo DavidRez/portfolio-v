29fe12ed65ea78625a3edb1af89a518d684275f7
David Ramirez <davidraviles@gmail.com>
2019-06-21 00:52:17 -0700

    added style for loader spinner

---
+++ head.php
@@ -0,0 +1,10 @@
+<meta content="width=device-width, initial-scale=1" name="viewport" />
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+<link rel="stylesheet"  type="text/css" href="/style/main.css">
+<link rel="stylesheet"  type="text/css" href="/style/nav.css">
+<link rel="stylesheet"  type="text/css" href="/style/titlebar.css">
+<link rel="stylesheet"  type="text/css" href="/style/work.css">
+<link rel="stylesheet"  type="text/css" href="/style/lightbox.css">
+<link rel="stylesheet"  type="text/css" href="/style/showcase.css">
+<link rel="stylesheet"  type="text/css" href="/style/mSpinnerSignal.css">
+<title>The Work of David Ramirez</title>
