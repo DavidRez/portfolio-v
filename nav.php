e4802d477099b4d706631c95fa636de6b5011897
David Ramirez <davidraviles@gmail.com>
2019-06-21 10:55:43 -0700

    Converted to php file.

---
+++ nav.php
@@ -0,0 +1,9 @@
+<div id="nav">
+    <ul>
+        <li><a data-type="all" href="/">ALL</a></li>
+        <li><a data-type="web" href="/web">WEB</a></li>
+        <li><a data-type="photo" href="/photo">PHOTO</a></li>
+        <li><a data-type="illustration" href="/illustration">ILLUSTRATION</a></li>
+        <li><a data-type="other" href="/other">OTHER</a></li>
+    </ul>
+</div>
---
+++ titlebar.php
@@ -0,0 +1,13 @@
+<div id="titlewrap">
+    <div id="titlebar">
+    <div id="logo"><img src="/img/owl.svg" alt="owl"></div>
+    <div id="title">DAVID RAMIREZ</div>
+    <div id="icons">
+        <ul>
+            <li><a href="mailto:davidraviles@gmail.com"><i class="far fa-envelope"></i></a></li>
+            <li><a href="https://www.linkedin.com/in/davidraviles" target="_blank"><i class="fab fa-linkedin-in"></i></a></li>
+            <li><a href="https://www.github.com/davidrez" target="_blank"><i class="fab fa-github"></i></a></li>
+        </ul>
+    </div>
+    </div>
+</div>
