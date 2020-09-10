d5990389b598a3316d13e40906e42d6697f64be3
David Ramirez <davidraviles@gmail.com>
2020-09-10 01:16:56 -0700

    Reorganized files for React project.

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  entry: __dirname + "/client/index.jsx",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      }
+    ]
+  },
+  output: {
+    filename: "bundle.js",
+    path: __dirname + "/public",
+    publicPath: "/public"
+  }
+};
